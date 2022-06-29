1532b1881f59d9abc749021c3704be1805592ef9
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-06-29 13:24:04 +0530

    Add files via upload

---
+++ modules/__main__.py
@@ -0,0 +1,78 @@
+from modules.clientbot.clientbot import pytgcalls, client, client1, client2, client3, client4, client5, client6, client7, client8, client9
+from pyrogram import idle
+
+if client:
+   pytgcalls.start()
+   client.join_chat("Altron_X")
+   client.join_chat("HeroOfficialBots")
+   client.join_chat("yaaro_ki_yaarii")
+   client.join_chat("TheAltron")
+
+if client1:
+   client1.start()
+   client1.join_chat("Altron_X")
+   client1.join_chat("HeroOfficialBots")
+   client1.join_chat("yaaro_ki_yaarii") 
+   client1.join_chat("TheAltron") 
+        
+if client2:
+   client2.start()
+   client2.join_chat("Altron_X")
+   client2.join_chat("HeroOfficialBots")
+   client2.join_chat("yaaro_ki_yaarii")
+   client2.join_chat("TheAltron")
+        
+if client3:
+   client3.start()
+   client3.join_chat("Altron_X")
+   client3.join_chat("HeroOfficialBots")
+   client3.join_chat("yaaro_ki_yaarii")
+   client3.join_chat("TheAltron")
+
+if client4:
+   client4.start()
+   client4.join_chat("Altron_X")
+   client4.join_chat("HeroOfficialBots")
+   client4.join_chat("yaaro_ki_yaarii")
+   client4.join_chat("TheAltron")
+
+if client5:
+   client5.start()
+   client5.join_chat("Altron_X")
+   client5.join_chat("HeroOfficialBots")
+   client5.join_chat("yaaro_ki_yaarii")
+   client5.join_chat("TheAltron")
+
+if client6:
+   client6.start()
+   client6.join_chat("Altron_X")
+   client6.join_chat("HeroOfficialBots")
+   client6.join_chat("yaaro_ki_yaarii")
+   client6.join_chat("TheAltron")
+
+if client7:
+   client7.start()
+   client7.join_chat("Altron_X")
+   client7.join_chat("HeroOfficialBots")
+   client7.join_chat("yaaro_ki_yaarii")
+   client7.join_chat("TheAltron")
+
+if client8:
+   client8.start()
+   client8.join_chat("Altron_X")
+   client8.join_chat("HeroOfficialBots")
+   client8.join_chat("yaaro_ki_yaarii")
+   client8.join_chat("TheAltron")
+
+if client9:
+   client9.start()
+   client9.join_chat("Altron_X")
+   client9.join_chat("HeroOfficialBots")
+   client9.join_chat("yaaro_ki_yaarii")
+   client9.join_chat("TheAltron")
+
+print("YOUR MUSIC x SPAM BOT DEPLOYED 💫🤟")
+print("GO TO TELEGRAM @TheAltron ⭐")
+idle()
+
+
